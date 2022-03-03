656d040873bb3b3966323382f8421c91661eb8bd
DebeshMishra <98782809+DebeshMishra@users.noreply.github.com>
2022-03-02 18:14:45 -0700

    Login logs table

--- src/main/java/com/cse545/hospitalSystem/controllers/LoginController.java
+++ src/main/java/com/cse545/hospitalSystem/controllers/LoginController.java
@@ -1,5 +1,11 @@
 package com.cse545.hospitalSystem.controllers;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,9 +21,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cse545.hospitalSystem.config.security.JwtTokenProvider;
+import com.cse545.hospitalSystem.models.Logs;
 import com.cse545.hospitalSystem.models.User;
 import com.cse545.hospitalSystem.models.ReqAndResp.AuthToken;
 import com.cse545.hospitalSystem.models.ReqAndResp.LoginRequestDTO;
+import com.cse545.hospitalSystem.repositories.LogsRepository;
 import com.cse545.hospitalSystem.services.UserService;
 
 /*
@@ -32,6 +40,9 @@ public class LoginController {
 	
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private LogsRepository logsRepo;
 	
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -49,6 +60,17 @@ public class LoginController {
                 		loginRequestDTO.getPassword()
                 )
         );
+    	
+    	Logger logger = LogManager.getLogger(LoginController.class);
+    	ZoneId zoneId = ZoneId.of("America/Phoenix");
+        LocalTime localTime=LocalTime.now(zoneId);
+        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        //String formattedTime=localTime.format(formatter);
+        LocalDate localDate = LocalDate.now();
+    	//logger.info("logged in user " + loginRequestDTO.getEmail() + " at time " + formattedTime + " on date "+localDate);
+    	Logs loginLog = new Logs(loginRequestDTO.getEmail(), localDate, localTime);
+    	logsRepo.save(loginLog);
+    	
         SecurityContextHolder.getContext().setAuthentication(authentication);
         final String token = jwtTokenProvider.generateToken(authentication);
         System.out.println(new AuthToken(token).toString());
