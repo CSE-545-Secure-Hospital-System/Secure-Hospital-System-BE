a94c1f1354ac70bb0559a1811518c3f932493200
SaicharanPapani <90519153+SaicharanPapani@users.noreply.github.com>
2022-03-15 20:08:34 -0700

    correctionss

--- src/main/java/com/cse545/hospitalSystem/controllers/AdminController.java
+++ src/main/java/com/cse545/hospitalSystem/controllers/AdminController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,10 +33,10 @@ public class AdminController {
 	private UserService userService;
     
 	@CrossOrigin
-	@PreAuthorize("hasRole('ADMIN')")
-	@RequestMapping(value="/getAllUsers", method = RequestMethod.GET)
-	public List<User> getAllUsers(){
-		return userService.getAllUser();
+//	@PreAuthorize("hasRole('ADMIN')")
+	@RequestMapping(value="/getAllUsers", method = RequestMethod.POST)
+	public List<User> getAllUsers(@RequestBody String searchTerm){
+		return userService.getAllUser(searchTerm);
 	}
 
 }
--- src/main/java/com/cse545/hospitalSystem/repositories/UserRepository.java
+++ src/main/java/com/cse545/hospitalSystem/repositories/UserRepository.java
@@ -1,10 +1,12 @@
 package com.cse545.hospitalSystem.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +23,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("UPDATE User a "+
     "SET a.enabled = TRUE WHERE a.email = ?1")
     int enableUser(String email);
+    
+    
+    @Query("FROM User u WHERE (u.firstName LIKE %?1% OR u.lastName LIKE %?1% OR u.email LIKE %?1%)")
+    List<User> searchByTerm(@Param("searchTerm") String searchTerm);
 
 
 }
