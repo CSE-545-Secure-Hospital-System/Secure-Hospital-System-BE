36e9c466b04991a63df6be720e9281a059a00738
glend17 <glendsouza1702@gmail.com>
2022-03-29 22:55:54 -0700

    forgot password implementation

---
+++ src/main/java/com/cse545/hospitalSystem/models/ReqAndResp/ForgotPasswordRequestDTO.java
@@ -0,0 +1,16 @@
+package com.cse545.hospitalSystem.models.ReqAndResp;
+
+public class ForgotPasswordRequestDTO {
+    
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
