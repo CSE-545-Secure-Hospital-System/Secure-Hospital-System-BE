e0d5ed6440eecf7b052c4fd1db3a2cb5cfd7e03f
glend17 <glendsouza1702@gmail.com>
2022-04-01 14:39:37 -0700

    Update User.java

--- src/main/java/com/cse545/hospitalSystem/models/User.java
+++ src/main/java/com/cse545/hospitalSystem/models/User.java
@@ -115,14 +115,14 @@ public class User implements UserDetails {
     @JoinColumn(name = "BILL_ID") })
     private Set<Bill> bills;
     
-//    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-//    @JoinTable(name = "USER_TRANSACTION",
-//    joinColumns = {
-//    @JoinColumn(name = "USER_ID")
-//    },
-//    inverseJoinColumns = {
-//    @JoinColumn(name = "TRANSACTION_ID") })
-//    private Set<Transaction> transactions;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "USER_TRANSACTION",
+    joinColumns = {
+    @JoinColumn(name = "USER_ID")
+    },
+    inverseJoinColumns = {
+    @JoinColumn(name = "TRANSACTION_ID") })
+    private Set<Transaction> transactions;
     
     
     public void addAppointment(Appointment a) {
