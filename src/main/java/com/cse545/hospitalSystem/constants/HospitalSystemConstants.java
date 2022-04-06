21bbfc61a9ec65a227ed6102856f51578556c40f
DebeshMishra <98782809+DebeshMishra@users.noreply.github.com>
2022-04-06 15:27:36 -0700

    Update HospitalSystemConstants.java

--- src/main/java/com/cse545/hospitalSystem/constants/HospitalSystemConstants.java
+++ src/main/java/com/cse545/hospitalSystem/constants/HospitalSystemConstants.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class HospitalSystemConstants {
 	
 	public static final Integer OTP_EXPIRES_IN = 2;
-	public static String URL = "http://localhost:8080/api/auth/confirm?token=";
+	public static String URL = "https://shscse545group3.herokuapp.com/api/auth/confirm?token=";
 	public static String subject = "Confirmation Email";
 
 }
